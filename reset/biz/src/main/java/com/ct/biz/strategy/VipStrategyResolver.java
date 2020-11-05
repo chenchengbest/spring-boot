c9cdc5b9bd5b921e5d8046c6b846f62e35e70684
chen.cheng <0027010605@iwhalecloud.com>
2020-11-05 16:17:56 +0800

    fix：完善异常处理

--- reset/biz/src/main/java/com/ct/biz/strategy/VipStrategyResolver.java
+++ reset/biz/src/main/java/com/ct/biz/strategy/VipStrategyResolver.java
@@ -1,16 +1,19 @@
 package com.ct.biz.strategy;
 
-import com.ct.common.annotation.Log;
-import com.ct.common.annotation.VipStrategy;
-import com.ct.common.exception.BusinessException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.ct.common.annotation.Log;
+import com.ct.common.annotation.VipStrategy;
+import com.ct.common.exception.Assert;
+import com.ct.common.exception.BusinessException;
+import com.ct.common.exception.ErrorCode;
 
 /**
  * The type Vip strategy resolver.
@@ -69,9 +72,8 @@ public class VipStrategyResolver implements InitializingBean, ApplicationContext
      */
     @Log(title = "策略器门面",action = "getHandler")
     public IVipStrategy getHandler(String vipCode) throws BusinessException {
-        if (!vipStrategyResolverMap.containsKey(vipCode)) {
-            throw new BusinessException(String.format("不存在类型为：{%s}策略", vipCode));
-        }
+        Assert.isTrue(!vipStrategyResolverMap.containsKey(vipCode), ErrorCode.PARAM_ERROR.getCode(),
+            String.format("不存在类型为：{%s}策略", vipCode));
         return vipStrategyResolverMap.get(vipCode);
     }
 }
